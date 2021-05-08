439514f3edef0ea6a3cc4b63062e6b5ac1b3fc9a
wipeautcrafter <wipeautcrafter@gmail.com>
2021-05-08 14:29:16 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ echo '[#] creating systemd service'
 echo 'generating service file'
 echo '[Unit]' > lichtkrant.service
 echo 'Description=lichtkrant systemd service' >> lichtkrant.service
+echo 'After=network-online.target' >> lichtkrant.service
+echo 'Wants=network-online.target' >> lichtkrant.service
 echo '' >> lichtkrant.service
 echo '[Service]' >> lichtkrant.service
 echo 'Type=simple' >> lichtkrant.service
@@ -27,6 +29,7 @@ echo 'WantedBy=multi-user.target' >> lichtkrant.service
 echo 'installing service'
 sudo mv lichtkrant.service /etc/systemd/system
 sudo systemctl daemon-reload
+sudo systemctl enable lichtkrant
 
 echo 'starting service'
 sudo systemctl restart lichtkrant
