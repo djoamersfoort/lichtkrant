1767ecd309bc401dfe710797199f0feb7d92817c
wipeautcrafter <wipeautcrafter@gmail.com>
2021-05-15 12:03:26 +0200

    added nyan

---
+++ states/idle/nyan.mod.py
@@ -0,0 +1,40 @@
+from PIL import Image
+from time import sleep
+from sys import stdout
+
+# module information
+name = "nyan"
+index = 0
+delay = 12
+
+
+# check function
+def check(_state):
+    return True
+
+
+def get_image(path):
+    image = Image.open(path)
+    image.resize((96, 32))
+
+    sequence = []
+
+    try:
+        while True:
+            temp = image.copy()
+            sequence.append(temp.convert('RGB'))
+            image.seek(len(sequence))
+    except EOFError:
+        pass
+
+    return sequence
+
+
+# module runner
+def run(_state):
+    sequence = get_image('static/nyan.gif')
+
+    while True:
+        for frame in sequence:
+            [stdout.buffer.write(bytes(a)) for a in frame.getdata()]
+            sleep(0.05)
