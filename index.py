8028d2d07ff86bc2387ca77ef9ae16c399af37c5
Ronald Moesbergen <github@rmoesbergen.nl>
2021-05-29 10:38:09 +0200

    fix: use class feat: add 'context' so data can be passed from init() to run()

--- index.py
+++ index.py
@@ -11,113 +11,117 @@ from time import sleep
 from glob import glob
 from multiprocessing import Process
 
-# parsing command-line arguments
-parser = argparse.ArgumentParser(
-    description='A driver for the DJO Lichtkrant project.')
 
-parser.add_argument('-m', '--module', default=None,
-                    help='load a specific module by name')
-parser.add_argument('-s', '--state-dir', default='./states',
-                    help='path to the states directory')
-parser.add_argument('-r', '--recursive', type=bool,
-                    default=True, help='whether to search recursively')
-parser.add_argument('-d', '--dry', action='store_true',
-                    default=False, help='do not spew out pixel data')
-parser.add_argument('-o', '--offline', action='store_true',
-                    default=False, help='disable MQTT connectivity')
+class LichtKrant:
 
-args = parser.parse_args()
+    def __init__(self, args):
+        self.module_context = {}
+        self.args = args
+        mqtt.connect(not args.offline)
 
+    def import_module(self, loc):
+        name = path.basename(loc).replace('.mod.py', '', -1)
+        spec = importlib.util.spec_from_file_location(name, loc)
 
-def import_module(loc):
-    name = path.basename(loc).replace('.mod.py', '', -1)
-    spec = importlib.util.spec_from_file_location(name, loc)
+        module = spec.loader.load_module()
+        if hasattr(module, 'init'):
+            context = module.init()
+            self.module_context[module.name] = context
+        return module
 
-    module = spec.loader.load_module()
-    if hasattr(module, 'init'):
-        module.init()
-    return module
+    def read_dir(self, location):
+        # loading state modules
+        return [self.import_module(file) for file in glob(location, recursive=self.args.recursive)]
 
+    def get_state(self, states, space_state):
+        # getting highest indexed state
+        if self.args.module is not None:
+            try:
+                return [s for s in states if s.name == args.module][0]
+            except IndexError:
+                raise Exception('The module passed does not exist.')
 
-def read_dir(location):
-    # loading state modules
-    return [import_module(file) for file in glob(
-        location, recursive=args.recursive)]
+        # filter states
+        filtered_states = [s for s in states if s.check(space_state)]
 
+        # return random with highest index
+        random.shuffle(filtered_states)
 
-def get_state(states, space_state):
-    # getting highest indexed state
-    if args.module is not None:
-        try:
-            return [s for s in states if s.name == args.module][0]
-        except IndexError:
-            raise Exception('The module passed does not exist.')
+        if not filtered_states:
+            return None
 
-    # filter states
-    filtered_states = [s for s in states if s.check(space_state)]
+        return sorted(filtered_states, key=lambda s: s.index, reverse=True)[0]
 
-    # return random with highest index
-    random.shuffle(filtered_states)
+    def run_state(self, state):
+        # running states
+        if self.args.dry:
+            print(f"state: {state.name}")
+            return None
 
-    if not filtered_states:
-        return None
+        states = mqtt.get_states()
+        context = self.module_context[state.name] if state.name in self.module_context else None
+        if context is not None:
+            process = Process(target=state.run, args=(states, context))
+        else:
+            process = Process(target=state.run, args=(states,))
+        process.start()
 
-    return sorted(filtered_states, key=lambda s: s.index, reverse=True)[0]
+        return process
 
+    def state_loop(self):
+        # read all modules
+        state_modules = self.read_dir(self.args.state_dir + '**/**/*.mod.py')
 
-def run_state(state):
-    # running states
-    if args.dry:
-        print(f"state: {state.name}")
-        return None
-
-    states = mqtt.get_states()
-    process = Process(target=state.run, args=[states])
-    process.start()
-
-    return process
-
-
-def state_loop():
-    # read all modules
-    state_modules = read_dir(args.state_dir + '**/**/*.mod.py')
-
-    # the state update loop
-    current_state = None
-    current_process = None
-
-    while True:
-        space_state = mqtt.get_states()
-        new_state = get_state(state_modules, space_state)
-
-        if new_state != current_state:
-            current_state = new_state
-
-            if current_process is not None:
-                current_process.terminate()
-                sleep(1)  # sleep to reset outlining
-
-            if current_state is not None:
-                current_process = run_state(current_state)
-
-        # delay or force update if neccesary
-        end_time = time.time() + new_state.delay
+        # the state update loop
+        current_state = None
+        current_process = None
 
         while True:
-            if time.time() >= end_time:
-                break
+            space_state = mqtt.get_states()
+            new_state = self.get_state(state_modules, space_state)
 
-            diff_state = get_state(state_modules, space_state)
+            if new_state != current_state:
+                current_state = new_state
 
-            if diff_state.index > new_state.index:
-                break
+                if current_process is not None:
+                    current_process.terminate()
+                    sleep(1)  # sleep to reset outlining
 
-            sleep(4)
+                if current_state is not None:
+                    current_process = self.run_state(current_state)
+
+            # delay or force update if necessary
+            end_time = time.time() + new_state.delay
+
+            while True:
+                if time.time() >= end_time:
+                    break
+
+                diff_state = self.get_state(state_modules, space_state)
+
+                if diff_state.index > new_state.index:
+                    break
+
+                sleep(4)
+
+    def start(self):
+        try:
+            self.state_loop()
+        except KeyboardInterrupt:
+            pass  # no ugly error message
 
 
-mqtt.connect(not args.offline)
+if __name__ == '__main__':
+    # parsing command-line arguments
+    parser = argparse.ArgumentParser(description='A driver for the DJO Lichtkrant project.')
 
-try:
-    state_loop()
-except KeyboardInterrupt:
-    pass  # no ugly error message
+    parser.add_argument('-m', '--module', default=None, help='load a specific module by name')
+    parser.add_argument('-s', '--state-dir', default='./states', help='path to the states directory')
+    parser.add_argument('-r', '--recursive', type=bool, default=True, help='whether to search recursively')
+    parser.add_argument('-d', '--dry', action='store_true', default=False, help='do not spew out pixel data')
+    parser.add_argument('-o', '--offline', action='store_true', default=False, help='disable MQTT connectivity')
+
+    args = parser.parse_args()
+
+    lichtkrant = LichtKrant(args)
+    lichtkrant.start()
