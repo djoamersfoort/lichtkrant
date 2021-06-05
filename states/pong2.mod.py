fe38957806d3b11ed456fa3fba7f99a40c184d0f
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2021-06-05 22:22:45 +0200

    pong is now playable

--- states/pong2.mod.py
+++ states/pong2.mod.py
@@ -1,9 +1,14 @@
 import threading
 import socket
 from random import choice
+from time import sleep
 import math
 
 from states.base import BaseState
+
+WHITE = [255, 255, 255]
+BLUE = [0, 0, 255]
+GREEN = [0, 255, 0]
 
 
 def number(num):
@@ -35,69 +40,93 @@ class Game:
         self.dims = dims
         h = self.dims["height"]
         w = self.dims["width"]
-        self.p1 = Player(x=1, y=int((h / 2) - Player.height / 2), limit=h - 1)
-        self.p2 = Player(x=w - 2, y=int((h / 2) - Player.height / 2), limit=h - 1)
+        self.p1 = Player(x=1, limit=h)
+        self.p2 = Player(x=w - 2, limit=h)
         self.ball = Ball(self.dims)
-        # TODO wait with moving the ball till there are two players
+
+    def start(self):
+        self.ball.reset()
+        self.p1.reset()
+        self.p2.reset()
 
     def update(self):
+        if not self.p1.ishuman and not self.p2.ishuman:
+            return
+        if self.p1.has_won or self.p2.has_won:
+            sleep(5)
+            self.start()
+            return
         self.p1.move()
         self.p2.move()
         self.ball.move()
-        # update scoring
+        # bounce on the players
+        if round(self.ball.x) == 1:
+            bounce_pos = self.ball.y - self.p1.y
+            if bounce_pos >= 0 and bounce_pos <= self.p1.height:
+                steps = self.p1.height + 2
+                self.ball.direction = (bounce_pos + 1) * (180 / steps)
+                self.ball.velocity *= 1.1
+                return
+        elif round(self.ball.x) == self.dims["width"] - 2:
+            bounce_pos = self.ball.y - self.p2.y
+            if bounce_pos >= 0 and bounce_pos <= self.p2.height:
+                steps = self.p2.height + 2
+                self.ball.direction = 360 - ((bounce_pos + 1) * (180 / steps))
+                self.ball.velocity *= 1.1
+                return
+        # assign points, skipped if the ball was bounced above
         if self.ball.x < 0:
             self.p2.score += 1
             self.ball.reset("left")
-        elif round(self.ball.x) == self.dims["width"] - 1:
+        elif round(self.ball.x) >= self.dims["width"] - 1:
             self.p1.score += 1
             self.ball.reset("right")
         # check winner
-        # TODO check for 11 points and 2 points diff
-        # bounce on the players
-        if round(self.ball.x) == 1:
-            # TODO bounce back and increase velocity
-            pass
-        elif round(self.ball.x) == self.dims['width'] - 2:
-            # TODO bounce back and increase velocity
-            pass
+        if self.p1.score >= 11 and self.p1.score - self.p2.score >= 2:
+            self.p1.has_won = True
+        if self.p2.score >= 11 and self.p2.score - self.p1.score >= 2:
+            self.p2.has_won = True
 
 
 class Player:
-    height = 6
-    def __init__(self, x, y, limit):
+    def __init__(self, x, limit):
+        self.height = 7
         self.x = x
-        self.y = y
-        self.score = 0
         self.limit = limit
         self.ishuman = False
+        self.reset()
+
+    def reset(self):
+        self.y = int((self.limit / 2) - self.height / 2)
+        self.score = 0
+        self.has_won = False
         self.movement = 0
 
     def move(self):
         self.y += self.movement
-        self.y = min(max(0, self.y), self.limit - self.height + 1)
+        self.y = min(max(0, self.y), self.limit - self.height)
+
+    def color(self):
+        if self.has_won:
+            return GREEN
+        elif self.ishuman:
+            return BLUE
+        return WHITE
 
 
 class Ball:
     def __init__(self, bounds):
-        self.velocity = 1
         self.bounds = bounds
         self.reset()
 
     def move(self):
         self.x += math.sin(math.radians(self.direction)) * self.velocity
         self.y += math.cos(math.radians(self.direction)) * self.velocity
-        if round(self.y) <= 0:
-            if self.direction < 90:
-                self.direction -= 90
-            elif self.direction > 90:
-                self.direction += 90
-        if round(self.y) >= self.bounds['height'] - 1:
-            if self.direction < 180:
-                self.direction -= 90
-            elif self.direction > 180:
-                self.direction -= 90
+        if round(self.y) <= 0 or round(self.y) >= self.bounds["height"] - 1:
+            self.direction = 180 - self.direction
 
     def reset(self, direction=None):
+        self.velocity = 1
         directions = {"left": [45, 135], "right": [225, 315]}
         self.direction = choice(directions.get(direction, [45, 135, 225, 315]))
         self.x = self.bounds["width"] / 2
@@ -112,11 +141,6 @@ class State(BaseState):
 
     winw = 96
     winh = 32
-
-    white = [255, 255, 255]
-    blue = [0, 0, 255]
-    red = [255, 0, 0]
-    green = [0, 255, 0]
 
     def __init__(self):
         super().__init__()
@@ -138,32 +162,36 @@ class State(BaseState):
                 # draw all elements
                 for player in [self.game.p1, self.game.p2]:
                     for y in range(player.y, player.y + player.height):
-                        if player.ishuman:
-                            pixels[y][player.x] = self.blue
-                        else:
-                            pixels[y][player.x] = self.white
+                        pixels[y][player.x] = player.color()
                 ball = self.game.ball
-                ball_x = min(max(0, ball.x), self.game.dims['width'])
-                ball_y = min(max(0, ball.y), self.game.dims['height'] - 1)
-                pixels[round(ball_y)][round(ball_x)] = self.white
+                ball_x = min(max(0, ball.x), self.game.dims["width"] - 1)
+                ball_y = min(max(0, ball.y), self.game.dims["height"] - 1)
+                pixels[round(ball_y)][round(ball_x)] = WHITE
                 for y in range(0, self.winh):
                     if y % 2 == 0:
-                        pixels[y][int(self.winw / 2)] = self.white
-                score_left_x = round(self.game.dims['width'] / 4) - 1
+                        pixels[y][int(self.winw / 2)] = WHITE
+                score_left_x = round(self.game.dims["width"] / 4) - 1
                 for y, row in enumerate(number(self.game.p1.score)):
                     for x, pixel in enumerate(row):
                         if pixel:
-                            pixels[y + 1][score_left_x + x] = self.white
-                score_right_x = round(self.game.dims['width'] / 4 * 3) - 1
+                            if self.game.p1.has_won:
+                                pixels[y + 1][score_left_x + x] = GREEN
+                            else:
+                                pixels[y + 1][score_left_x + x] = WHITE
+                score_right_x = round(self.game.dims["width"] / 4 * 3) - 1
                 for y, row in enumerate(number(self.game.p2.score)):
                     for x, pixel in enumerate(row):
                         if pixel:
-                            pixels[y + 1][score_right_x + x] = self.white
+                            if self.game.p2.has_won:
+                                pixels[y + 1][score_right_x + x] = GREEN
+                            else:
+                                pixels[y + 1][score_right_x + x] = WHITE
                 # flatten, convert and write buffer to display
                 self.output_frame(bytes(flatten(pixels)))
+                sleep(1 / 30)
 
     def receive(self):
-        HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+        HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
         PORT = 9999  # Port to listen on (non-privileged ports are > 1023)
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -182,9 +210,9 @@ class State(BaseState):
             try:
                 data = conn.recv(1)
             except Exception:
-                if player == '1':
+                if player == "1":
                     self.game.p1.ishuman = False
-                if player == '2':
+                if player == "2":
                     self.game.p2.ishuman = False
                 break
             if data.decode() == "":
@@ -194,47 +222,51 @@ class State(BaseState):
                     self.game = Game({"width": self.winw, "height": self.winh})
 
                 if player is None:
-                    if data.decode() == '1' or data.decode() == '2':
-                        if data.decode() == '1':
+                    if data.decode() == "1" or data.decode() == "2":
+                        if data.decode() == "1":
                             if self.game.p1.ishuman:
                                 msg = "redSomeone is already player 1"
                             else:
                                 self.game.p1.ishuman = True
                                 msg = "yellowYou are player 1"
                                 player = "1"
-                        if data.decode() == '2':
+                                if self.game.p2.ishuman:
+                                    self.game.start()
+                        if data.decode() == "2":
                             if self.game.p2.ishuman:
                                 msg = "redSomeone is already player 2"
                             else:
                                 self.game.p2.ishuman = True
                                 msg = "yellowYou are player 2"
                                 player = "2"
+                                if self.game.p1.ishuman:
+                                    self.game.start()
                     else:
                         msg = "redUnknown"
 
                 else:
-                    if data.decode() == 'w':
+                    if data.decode() == "w":
                         msg = "greenup"
-                        if player == '1':
+                        if player == "1":
                             self.game.p1.movement = -1
                         else:
                             self.game.p2.movement = -1
 
-                    elif data.decode() == 's':
-                        if player == '1':
+                    elif data.decode() == "s":
+                        if player == "1":
                             self.game.p1.movement = 1
                         else:
                             self.game.p2.movement = 1
-                        msg = 'greendown'
+                        msg = "greendown"
                     else:
-                        msg = 'redUnknown'
+                        msg = "redUnknown"
 
             try:
                 conn.send(msg.encode())
             except Exception:
-                if player == '1':
+                if player == "1":
                     self.game.p1.ishuman = False
-                if player == '2':
+                if player == "2":
                     self.game.p2.ishuman = False
                 if not self.game.p1.ishuman and not self.game.p2.ishuman:
                     self.game = None
