a36ab9908dab6db62857903edbbc797d09823f1a
ILoveAndLikePizza <53319481+ILoveAndLikePizza@users.noreply.github.com>
2021-08-07 17:49:08 +0200

    Add files via upload

--- states/idle/gamen.mod.py
+++ states/idle/gamen.mod.py
@@ -44,7 +44,7 @@ class State(BaseState):
                 font = ImageFont.truetype(font_path, size=7)
                 for i in range(0, len(wall_of_shame)):
                     height = 17 if i % 2 == 0 else 26
-                    draw.text(((1 + i * 20) - (self.elapsed - self.delay / 2) * (len(wall_of_shame) + 2), height), wall_of_shame[i], fill="white", anchor="lm", font=font)
+                    draw.text(((1 + i * 20) - (self.elapsed - self.delay / 2) * (len(wall_of_shame) + 1), height), wall_of_shame[i], fill="white", anchor="lm", font=font)
 
             self.output_image(image)
             sleep(1)
