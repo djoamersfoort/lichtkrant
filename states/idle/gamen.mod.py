8a379fa1ad2e0b19bc43dbd63ba38b5955532d2f
ILoveAndLikePizza <53319481+ILoveAndLikePizza@users.noreply.github.com>
2021-08-08 13:44:17 +0200

    Niet gamen voor negen uur (#11)
    * Niet gamen voor negen uur
    
    * Niet gamen voor negen uur
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- states/idle/gamen.mod.py
+++ states/idle/gamen.mod.py
@@ -4,14 +4,13 @@ from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 import requests
 
-
 class State(BaseState):
     # module information
     name = "gamen"
     index = 0
     delay = 16
     elapsed = 0
-
+    
     # module check function
     def check(self, _state):
         dt = datetime.now()
@@ -19,7 +18,7 @@ class State(BaseState):
 
     def run(self):
         font_path = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf"
-
+        
         while not self.killed:
             response = requests.get("https://nm-games.eu/games/djo-game-register.json").json()
             keer_gegamed = str(response["keer_gegamed"])
@@ -27,7 +26,7 @@ class State(BaseState):
 
             image = Image.new("RGB", (96, 32), (0, 0, 50))
             draw = ImageDraw.Draw(image)
-
+        
             if self.elapsed < self.delay / 2:
                 fsize = 14 if len(keer_gegamed) >= 3 else 18
                 font = ImageFont.truetype(font_path, size=fsize)
@@ -35,16 +34,17 @@ class State(BaseState):
 
                 font = ImageFont.truetype(font_path, size=8)
                 nm_games = "N&M Game" if keer_gegamed == 1 else "N&M Games"
+                before_time = "21:30!" if datetime.now().weekday() == 4 else "13:00!"
                 draw.text((63, 7), nm_games, fill="white", anchor="mm", font=font)
                 draw.text((63, 16), "gespeeld vóór", fill="white", anchor="mm", font=font)
-                draw.text((63, 25), "game tijd!", fill="white", anchor="mm", font=font)
+                draw.text((63, 25), before_time, fill="white", anchor="mm", font=font)
             else:
                 font = ImageFont.truetype(font_path, size=12)
-                draw.text((49, 1), "WALL OF SHAME", fill="orange", anchor="mt", font=font)
+                draw.text((49, 1), "WALL OF SHAME", fill=(230, 30, 230), anchor="mt", font=font)
                 font = ImageFont.truetype(font_path, size=7)
                 for i in range(0, len(wall_of_shame)):
                     height = 17 if i % 2 == 0 else 26
-                    draw.text(((1 + i * 20) - (self.elapsed - self.delay / 2) * 7, height), wall_of_shame[i], fill="white", anchor="lm", font=font)
+                    draw.text(((1 + i * 20) - (self.elapsed - self.delay / 2) * (len(wall_of_shame) + 1), height), wall_of_shame[i], fill="white", anchor="lm", font=font)
 
             self.output_image(image)
             sleep(1)
