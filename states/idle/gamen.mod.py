7f4efdea8c39eb95632df0da65ee773cdd87c4e4
ILoveAndLikePizza <53319481+ILoveAndLikePizza@users.noreply.github.com>
2021-08-14 09:41:48 +0200

    Add files via upload

--- states/idle/gamen.mod.py
+++ states/idle/gamen.mod.py
@@ -14,13 +14,16 @@ class State(BaseState):
     # module check function
     def check(self, _state):
         dt = datetime.now()
-        return (dt.weekday() == 4 and (dt.hour < 21 or (dt.hour == 21 and dt.minute < 30))) or (dt.weekday() == 5 and dt.hour < 13)
+        if dt.weekday() == 4: # friday
+            return dt.hour < 21 or (dt.hour == 21 and dt.minute < 30)
+        elif dt.weekday() == 5: # saturday
+            return dt.hour < 13
 
     def run(self):
         font_path = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf"
         
         while not self.killed:
-            response = requests.get("https://nm-games.eu/games/djo-game-register.json").json()
+            response = requests.get("https://nm-games.eu/media/djo-game-register.json").json()
             keer_gegamed = str(response["keer_gegamed"])
             wall_of_shame = response["wall_of_shame"]
 
@@ -33,8 +36,8 @@ class State(BaseState):
                 draw.text((15, 16), keer_gegamed, fill="white", anchor="mm", font=font)
 
                 font = ImageFont.truetype(font_path, size=8)
-                nm_games = "N&M Game" if keer_gegamed == 1 else "N&M Games"
-                before_time = "21:30!" if datetime.now().weekday() == 4 else "13:00!"
+                nm_games = "N&M Game" if keer_gegamed == "1" else "N&M Games"
+                before_time = "13:00!" if datetime.now().weekday() == 5 else "21:30!"
                 draw.text((63, 7), nm_games, fill="white", anchor="mm", font=font)
                 draw.text((63, 16), "gespeeld vóór", fill="white", anchor="mm", font=font)
                 draw.text((63, 25), before_time, fill="white", anchor="mm", font=font)
@@ -44,7 +47,7 @@ class State(BaseState):
                 font = ImageFont.truetype(font_path, size=7)
                 for i in range(0, len(wall_of_shame)):
                     height = 17 if i % 2 == 0 else 26
-                    draw.text(((1 + i * 20) - (self.elapsed - self.delay / 2) * (len(wall_of_shame) + 1), height), wall_of_shame[i], fill="white", anchor="lm", font=font)
+                    draw.text(((1 + i * 20) - (self.elapsed - self.delay / 2) * len(wall_of_shame), height), wall_of_shame[i], fill="white", anchor="lm", font=font)
 
             self.output_image(image)
             sleep(1)
