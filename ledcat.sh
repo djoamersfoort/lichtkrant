b9ee8d10d4da1f8550906750f2af0a8c137b3ef8
Sverre Benedictus <sverrebenedictus@pop-os.localdomain>
2021-10-09 14:18:18 +0200

    Added snake online game

--- ledcat.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# add --color-correction srgb after ledcat for color correction
-sudo ./ledcat --geometry 96x32 rpi-led-matrix --led-cols 32 --led-rows 16 --led-chain 3 --led-parallel 2 --led-pwm-bits 8 --led-pwm-lsb-nanoseconds 500 --led-scan-mode progressive
