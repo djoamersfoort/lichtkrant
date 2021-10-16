20e009d8f8f6cb025938bf56a7be6c4995ea5409
ILoveAndLikePizza <53319481+ILoveAndLikePizza@users.noreply.github.com>
2021-10-16 11:10:02 +0200

    Read file once every 10s; add try-except to file read

--- states/idle/gamen.mod.py
+++ states/idle/gamen.mod.py
@@ -10,31 +10,36 @@ class State(BaseState):
     index = 0
     delay = 16
     elapsed = 0
+    check_elapsed = 0
 
     def get_response(self):
-        response = requests.get("https://nm-games.eu/media/djo-game-register.json").json()
-        return response["keer_gegamed"], response["wall_of_shame"]
+        try:
+            response = requests.get("https://nm-games.eu/media/djo-game-register.json").json()
+            return response["keer_gegamed"], response["wall_of_shame"]
+        except requests.exceptions.RequestException:
+            return 0, []
 
     # module check function
     def check(self, _state):
-        keer_gegamed, _ = self.get_response()
-        if keer_gegamed < 1:
-            return False
+        self.check_elapsed += 1
+        if self.check_elapsed % 10 == 0:
+            keer_gegamed, _ = self.get_response()
+            if keer_gegamed < 1:
+                return False
 
-        dt = datetime.now()
-        if dt.weekday() == 4: # friday
-            return dt.hour < 21 or (dt.hour == 21 and dt.minute < 30)
-        elif dt.weekday() == 5: # saturday
-            return dt.hour < 13
+            dt = datetime.now()
+            if dt.weekday() == 4: # friday
+                return dt.hour < 21 or (dt.hour == 21 and dt.minute < 30)
+            elif dt.weekday() == 5: # saturday
+                return dt.hour < 13
+        else: return False
 
     def run(self):
         font_path = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf"
 
         while not self.killed:
-            response = requests.get("https://nm-games.eu/media/djo-game-register.json").json()
-            keer_gegamed = str(response["keer_gegamed"])
-            wall_of_shame = response["wall_of_shame"]
-
+            keer_gegamed, wall_of_shame = self.get_response()
+            keer_gegamed = str(keer_gegamed)
 
             image = Image.new("RGB", (96, 32), (0, 0, 50))
             draw = ImageDraw.Draw(image)
