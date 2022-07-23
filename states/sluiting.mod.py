232965ce361c229898a70bc3a16fc17f849e403d
ILoveAndLikePizza <nathankoning@gmail.com>
2022-07-23 10:36:34 +0200

    Chore: set and restore volume of track

--- states/sluiting.mod.py
+++ states/sluiting.mod.py
@@ -1,5 +1,5 @@
 import requests
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 from time import sleep
 from math import floor
 from datetime import datetime
@@ -12,6 +12,7 @@ class State(BaseState):
     name = "sluiting"
     index = 9
     delay = 300
+    elapsed = 0
     scroll_x = 0
     # planets.png image width MUST be dividable by 96, else module won't work!
     solar_system_fragment_length = floor(Image.open("./static/planets.png").convert(mode="RGB").width / 96)
@@ -33,23 +34,34 @@ class State(BaseState):
 
     # start music
     def queue_song(self):
+        # set volume to max to make it hearable
+        requests.post(self.queue_uri, json={"jsonrpc": "2.0", "id": 1, "method": "core.mixer.set_volume", "params": {"volume": 100}})
+        # get tlid and play sluiting track
         response = requests.post(self.queue_uri, json=self.queue_data)
         tlid = response.json()["result"][0]["tlid"]
         requests.post(self.queue_uri, json={"jsonrpc": "2.0", "id": 1, "method": "core.playback.play", "params": {"tlid": tlid}})
+
+    def reset_volume(self):
+        requests.post(self.queue_uri, json={"jsonrpc": "2.0", "id": 1, "method": "core.mixer.set_volume", "params": {"volume": 35}})
 
     # module runner
     def run(self):
         if self.on_pi:
             try: self.queue_song()
             except: pass
-
+        font = ImageFont.truetype("/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf")
         while not self.killed:
             background = self.solar_systems[0].copy()
+            draw = ImageDraw.Draw(background)
+
             for i, img in enumerate(self.solar_systems):
                 background.paste(img, (-self.scroll_x + i * 96, 0))
             background.paste(self.solar_systems[0], (-self.scroll_x + len(self.solar_systems) * 96, 0))
+            draw.text((95, 31), str(self.elapsed), fill="white", anchor="rb", font=font)
             self.output_image(background)
             sleep(.1)
             self.scroll_x += 1
             if self.scroll_x >= self.solar_system_fragment_length * 96: self.scroll_x = 0
             
+            self.elapsed = round(self.elapsed + 0.1, 1)
+            if self.elapsed == 245: self.reset_volume() # 245 = 300 seconds in lichtkrant pace
