a7e71c122a7144d78c7f5032b1b78fe23cbad1e3
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2022-08-05 16:43:38 +0200

    fix: adjust enable check to new corvee API (fixes #59)

--- states/corvee.mod.py
+++ states/corvee.mod.py
@@ -35,7 +35,7 @@ class State(BaseState):
         elapsed = 0
         blink_invert = False
         names = self.get_names()
-        colors = [(randint(128, 255), randint(128, 255), randint(128, 255)) for i in names["present"]]
+        colors = [(randint(128, 255), randint(128, 255), randint(128, 255)) for _ in names["present"]]
         scroll_y = 0
         scroll_speed = 8
         font_path = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf"
@@ -49,7 +49,7 @@ class State(BaseState):
         while not self.killed:
             image = Image.new("RGB", (96, 32), (0, 0, 0))
             draw = ImageDraw.Draw(image)
-            
+
             if elapsed < 4:
                 draw.text((3, 2), "CORVEE", fill="white", anchor="lt", font=fonts["noto20"])
                 draw.text((92, 29), "tijd!", fill="white", anchor="rb", font=fonts["noto11"])
@@ -61,19 +61,22 @@ class State(BaseState):
             elif elapsed < 7:
                 draw.text((48, 16), "Wie o wie...", fill="yellow", anchor="mm", font=fonts["noto11"])
             elif elapsed < 10:
-                draw.text((48, 16), "...zijn de winnaars\nvan vandaag?", fill="yellow", anchor="mm", font=fonts["noto8"])
+                draw.text((48, 16), "...zijn de winnaars\nvan vandaag?", fill="yellow", anchor="mm",
+                          font=fonts["noto8"])
             elif elapsed < 13:
                 draw.text((48, 16), "We zullen\nhet zien!", fill="yellow", anchor="mm", font=fonts["noto11"])
             elif len(names["selected"]) > 0:
                 for i, j in enumerate(names["present"]):
                     draw.text((4, 5 + i * 12 - scroll_y), j, fill=colors[i], anchor="lm", font=fonts["noto11"])
-                    draw.text((4, 5 + i * 12 - scroll_y + len(names["present"]) * 12), j, fill=colors[i], anchor="lm", font=fonts["noto11"])
+                    draw.text((4, 5 + i * 12 - scroll_y + len(names["present"]) * 12), j, fill=colors[i], anchor="lm",
+                              font=fonts["noto11"])
                 scroll_y += scroll_speed
                 if scroll_y > len(names["present"]) * 12: scroll_y = 1
 
                 draw.rectangle([(0, 10), (95, 22)], fill=None, outline="cyan", width=1)
 
-                if scroll_speed > 1 and elapsed % 1 < .05: scroll_speed -= 1
+                if scroll_speed > 1 and elapsed % 1 < .05:
+                    scroll_speed -= 1
                 elif scroll_speed == 1 and (scroll_y + 10) % 12 == 0:
                     center_index = floor((scroll_y + 10) / 12)
                     if center_index == len(names["present"]): center_index = 0
@@ -95,7 +98,7 @@ class State(BaseState):
                     y = (elapsed + i * 8) % 40
                     draw.rectangle([(1, y - 8), (6, y - 4)], fill="blue")
                     draw.rectangle([(89, y - 8), (94, y - 4)], fill="blue")
-      
+
             self.output_image(image)
             sleep(.05)
             elapsed += .05
