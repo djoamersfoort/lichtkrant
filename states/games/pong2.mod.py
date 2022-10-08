cd5d8ef5c66824d6a76dff4cdc8163d9d725fee8
sverben <59171289+sverben@users.noreply.github.com>
2022-10-08 15:49:58 +0200

    Convert pong2

--- states/games/pong2.mod.py
+++ states/games/pong2.mod.py
@@ -1,10 +1,9 @@
 import math
-import socket
-import threading
 from random import choice
 from time import sleep, time
 
 from states.base import BaseState
+from states.socket import BasePlayer
 
 WHITE = [255, 255, 255]
 BLUE = [0, 0, 255]
@@ -17,25 +16,25 @@ class Game:
         self.dims = dims
         h = self.dims["height"]
         w = self.dims["width"]
-        self.players = []
-        self.players.append(Player(start_pos=0, bounds=(0, h)))
-        self.players.append(Player(start_pos=w - 1, bounds=(0, h)))
-        self.players.append(Player(start_pos=0, bounds=(0, 32), orientation="horizontal"))
-        self.players.append(Player(start_pos=0, bounds=(32, 64), orientation="horizontal"))
-        self.players.append(Player(start_pos=0, bounds=(64, 96), orientation="horizontal"))
-        self.players.append(Player(start_pos=h - 1, bounds=(0, 32), orientation="horizontal"))
-        self.players.append(Player(start_pos=h - 1, bounds=(32, 64), orientation="horizontal"))
-        self.players.append(Player(start_pos=h - 1, bounds=(64, 96), orientation="horizontal"))
+        self.boards = []
+        self.boards.append(Paddle(start_pos=0, bounds=(0, h)))
+        self.boards.append(Paddle(start_pos=w - 1, bounds=(0, h)))
+        self.boards.append(Paddle(start_pos=0, bounds=(0, 32), orientation="horizontal"))
+        self.boards.append(Paddle(start_pos=0, bounds=(32, 64), orientation="horizontal"))
+        self.boards.append(Paddle(start_pos=0, bounds=(64, 96), orientation="horizontal"))
+        self.boards.append(Paddle(start_pos=h - 1, bounds=(0, 32), orientation="horizontal"))
+        self.boards.append(Paddle(start_pos=h - 1, bounds=(32, 64), orientation="horizontal"))
+        self.boards.append(Paddle(start_pos=h - 1, bounds=(64, 96), orientation="horizontal"))
         self.ball = Ball(self.dims)
         self.reset()
 
     def reset(self):
         self.ball.reset()
-        for player in self.players:
+        for player in self.boards:
             player.reset()
 
     def check_collision(self, orientation, player_pos):
-        for player in self.players:
+        for player in self.boards:
             if player.orientation != orientation:
                 continue
             if orientation == "vertical":
@@ -69,9 +68,9 @@ class Game:
         self.ball.reset()
 
     def update(self):
-        if not self.players[0].ishuman or not self.players[1].ishuman:
+        if not self.boards[0].ishuman or not self.boards[1].ishuman:
             return
-        for player in self.players:
+        for player in self.boards:
             if player.has_won:
                 sleep(5)
                 self.reset()
@@ -88,12 +87,12 @@ class Game:
         if self.ball.y >= self.dims["height"]:
             self.check_collision("horizontal", self.dims["height"] - 1)
         # check winner
-        for player in self.players:
+        for player in self.boards:
             if player.score >= 11:
                 player.has_won = True
 
 
-class Player:
+class Paddle:
     def __init__(self, start_pos, bounds, orientation="vertical"):
         self.orientation = orientation
         self.height = 7
@@ -105,6 +104,7 @@ class Player:
         self.ishuman = False
         self.reset()
         self.code = BLUE
+        self.player = None
 
     def reset(self):
         average = (self.bounds[0] + self.bounds[1]) / 2
@@ -130,6 +130,25 @@ class Player:
         if self.ishuman:
             return self.code
         return WHITE
+
+
+class Player(BasePlayer):
+    def __init__(self, sio, sid, game):
+        super().__init__(sio, sid, game)
+        self.paddle = None
+
+    def on_press(self, key):
+        if key in ('w', 'a'):
+            self.paddle.movement = -1
+        elif key in ('s', 'd'):
+            self.paddle.movement = 1
+
+    def on_release(self, _key):
+        self.paddle.movement = 0
+
+    def on_leave(self):
+        self.paddle.player = None
+        self.paddle.ishuman = False
 
 
 class Ball:
@@ -166,7 +185,7 @@ class State(BaseState):
 
     def __init__(self):
         super().__init__()
-        threading.Thread(target=self.receive).start()
+        self.player_class = Player
 
     def check(self, _state):
         return self.game
@@ -189,7 +208,7 @@ class State(BaseState):
             ball_x = min(max(0, ball.x), self.game.dims["width"] - 1)
             ball_y = min(max(0, ball.y), self.game.dims["height"] - 1)
             pixels[round(ball_y)][round(ball_x)] = WHITE
-            for player in self.game.players:
+            for player in self.game.boards:
                 if not player.ishuman:
                     continue
                 if player.orientation == "vertical":
@@ -199,7 +218,7 @@ class State(BaseState):
                     for x in range(player.x, player.x + player.height):
                         pixels[player.y][x] = player.color()
             scores = []
-            for player in self.game.players:
+            for player in self.game.boards:
                 if not player.ishuman:
                     continue
                 if player.orientation == "vertical":
@@ -228,72 +247,19 @@ class State(BaseState):
                 time_delta = time_start - time_end
                 sleep(max(0.04 - time_delta, 0))
 
-    def receive(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-            s.bind(("0.0.0.0", 9999))
-            s.listen()
-            while not self.killed:
-                conn, addr = s.accept()
-                threading.Thread(target=self.msg, args=(conn, addr)).start()
-
-    def move(self, movement):
-        move = 0
-        if not len(movement) == 2:
-            return 0
-
-        if movement[0] == "1":
-            move -= 1
-        if movement[1] == "1":
-            move += 1
-
-        return move
+    def add_player(self, player):
+        if not self.game:
+            self.game = Game({"width": self.winw, "height": self.winh})
+        paddle = next(
+            (paddle for paddle in self.game.boards if not paddle.player), None)
+        if paddle:
+            paddle.player = player
+            paddle.ishuman = True
+            player.paddle = paddle
+        else:
+            return
 
     def color(self, code):
         r, g, b = tuple(int(code[i:i + 2], 16) for i in (0, 2, 4))
 
         return [r, g, b]
-
-    def msg(self, conn, _addr):
-        player = None
-        while not self.killed:
-            data = b''
-            if player:
-                try:
-                    data = conn.recv(7)
-                    # It is required to send text to find dead connections,
-                    # because 'recv' will happily continue without errors.
-                    # We send back some dummy data to detect closed sockets,
-                    # because empty strings are not enough or even send at all.
-                    # tldr: I kind of hate sockets now >.<
-                    conn.send(b"_")
-                except Exception:
-                    if player is not None:
-                        player.ishuman = False
-                        self.game.reset()
-                    total_players = 0
-                    for player in self.game.players:
-                        if player.ishuman:
-                            total_players += 1
-                    if total_players < 2:
-                        self.game = None
-                    break
-            if not self.game:
-                self.game = Game({"width": self.winw, "height": self.winh})
-            # We strip the data and then check if it contains any data.
-            # This is done to reset the movement only when new keys are send.
-            # Otherwise no movement would be possible at all,
-            # as real messages are frequently followed by an empty message.
-            request = data.decode().strip()
-            if player is not None:
-                if len(request) == 7 and request.startswith("#"):
-                    player.code = self.color(request.split("#")[1])
-                player.movement = self.move(request)
-            else:
-                for p in self.game.players:
-                    if not p.ishuman:
-                        player = p
-                        p.ishuman = True
-                        self.game.reset()
-                        break
