51a1f0f6a0ec880c79b400e717f96939d9649ff8
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2023-12-02 12:00:32 +0100

    chore: sync run.sh with version on device (#92)

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -x
-python3 index.py $@ | ./ledcat --geometry 96x32 rpi-led-matrix --led-cols 32 --led-rows 16 --led-chain 3 --led-parallel 2 --led-pwm-bits 8 --led-pwm-lsb-nanoseconds 500 --led-scan-mode progressive
+export PYTHONUNBUFFERED=1
+if [[ ! -f pipe ]]; then
+  mkfifo pipe
+fi
+
+python3 index.py $@ > pipe &
+./ledcat --input pipe --geometry 96x32 rpi-led-matrix --led-cols 32 --led-rows 16 --led-chain 3 --led-parallel 2 --led-pwm-bits 8 --led-pwm-lsb-nanoseconds 500 --led-scan-mode progressive
